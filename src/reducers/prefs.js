f8227e6e597ddefa853f312a24441662960dfc2a
Pavel Maslov <pavel.maslov@axfood.se>
2023-09-03 13:59:15 +0200

    save Settings state in Local Storage

--- src/reducers/prefs.js
+++ src/reducers/prefs.js
@@ -1,10 +1,20 @@
 import {createSlice} from '@reduxjs/toolkit';
 
+const beepOn = localStorage.getItem('beepOn');
+const crossHairOn = localStorage.getItem('crossHairOn');
+const bwOn = localStorage.getItem('bwOn');
+
+
+if (beepOn === null || crossHairOn === null || bwOn === null) {
+  localStorage.setItem('beepOn', 'true');
+  localStorage.setItem('crossHairOn', 'true');
+  localStorage.setItem('bwOn', 'true');
+}
 
 const initialState = {
-  beep: true,
-  crossHair: true,
-  bw: true
+  beep: localStorage.getItem('beepOn') === 'true',
+  crossHair: localStorage.getItem('crossHairOn')  === 'true',
+  bw: localStorage.getItem('bwOn') === 'true',
 };
 
 export const prefSlice = createSlice({
@@ -13,12 +23,15 @@ export const prefSlice = createSlice({
   reducers: {
     SET_BEEP: (state, action) => {
       state.beep = action.payload;
+      localStorage.setItem('beepOn', action.payload);
     },
     SET_CROSSHAIR: (state, action) => {
       state.crossHair = action.payload;
+      localStorage.setItem('crossHairOn', action.payload);
     },
     SET_BW: (state, action) => {
       state.bw = action.payload;
+      localStorage.setItem('bwOn', action.payload);
     }
   },
 });
